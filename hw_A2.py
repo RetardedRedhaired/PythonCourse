6538e02772f67d95b14ae9ec35acc03aede960ae
RetardedRedhaired <48159420+RetardedRedhaired@users.noreply.github.com>
2019-03-02 20:03:32 +0300

    Create hw_A2.py

---
+++ hw_A2.py
@@ -0,0 +1,26 @@
+def fio():
+    zubenko = 'hello'
+    mikhail = ' '
+    petrovich = 'world'
+    return zubenko + mikhail + petrovich
+def kem():
+    vor = 'hello'
+    v = ' '
+    zakone = 'world'
+    return vor + v + zakone
+def gde():
+    shumilovsky = 'hello '
+    gorodok = 'world'
+    return shumilovsky + gorodok
+def klichka():
+    mafioznik = 'hello world'
+    return mafioznik
+def uhhhblya():
+    uhhhblya = 'hello world'
+    return uhhhblya
+for i in range(2):
+    print(fio())
+    print(kem())
+    print(gde())
+    print(klichka())
+    print(uhhhblya())
