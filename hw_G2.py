3715b483669624be432b6b74345c4255af5ebb5f
RetardedRedhaired <48159420+RetardedRedhaired@users.noreply.github.com>
2019-04-26 21:30:11 +0300

    Update hw_G2.py

--- hw_G2.py
+++ hw_G2.py
@@ -1,9 +1,9 @@
-import math
+def prime(number):
+    c = range(1, number)
+    for a in c:
+        if (math.factorial(a-1) + 1) % a == 0:
+            yield a
+            
 
-def prime(n):
-    if (math.factorial(n-1) + 1) % n == 0:
-        return n
-n = input('Enter last number:')
-a = range(2, int(n))
-a = list(filter(prime,a))
-print(a)
+for curr in prime(20):
+    print(curr, end=' ')   
